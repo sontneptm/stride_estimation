61e9adebde31204fd712dbd9ccff23205a8bd290
Seungmin Jeong <sontneptm2@gmail.com>
2022-04-13 17:04:28 +0900

    update

--- file_reader.py
+++ file_reader.py
@@ -1,8 +1,18 @@
 from glob import glob
+import pandas as pd
+import numpy as np
 
-def read_files():
+def read_files() -> np.ndarray :
     files = glob('./stride_data/data/*')
-    print(files)
+    datas = []
+    for file in files:
+        data = pd.read_csv(file, usecols=(1,2,3,11,12)) # 1=x, 2=y, 3=z, 11=stride_len, 12=stride_num
+        print(data)
+        
+    return datas
 
 if __name__ == '__main__':
-    read_files()
+    data = read_files()
+
+    print(type(data))
+    print(data)
