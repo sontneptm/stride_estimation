191a21f9fab384019b005f42648ef496017de1c3
Seungmin Jeong <sontneptm2@gmail.com>
2022-04-13 17:09:01 +0900

    update

---
+++ stride_data.py
@@ -0,0 +1,7 @@
+
+class Stride():
+    def __init__(self, stride_length, x_list, y_list, z_list) -> None:
+        self.stride_length = stride_length
+        self.x_list = x_list
+        self.y_list = y_list
+        self.z_list = z_list
