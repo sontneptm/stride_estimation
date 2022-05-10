00c95ecbda0e6ea04b9f805d24cdcb8a13953dd4
Seungmin Jeong <sontneptm2@gmail.com>
2022-05-10 15:04:08 +0900

    update

--- custom_step_model.py
+++ custom_step_model.py
@@ -11,9 +11,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from glob import glob
 
-input_size = None
-batch_size = 32
-
 def setup_gpu():
     print("==== setting up GPU ====")
     gpus = tf.config.experimental.list_physical_devices('GPU')
@@ -23,94 +20,114 @@ def setup_gpu():
         except RuntimeError as e:
             print(e)
 
-def scale_data(train_data, val_data, test_data):
-    print("==== scaling DATA ====")
-    scaler = StandardScaler()
+class StepModel():
+    def __init__(self) -> None:
+        self.epochs = 1000
+        self.input_size = None
 
-    train_data = scaler.fit_transform(train_data)
-    val_data = scaler.transform(val_data)
-    test_data = scaler.transform(test_data)
+        self.train_dataset =None
+        self.val_dataset =None
+        self.test_dataset =None
 
-    return train_data, val_data, test_data
+        self.load_data()
+        self.build_cnn_model
 
-def load_data():
-    print("==== loading DATA ====")
-    data_path_list = glob('./stride_lab_data/processed_data/*/*')
+    def scale_data(self, train_data, val_data, test_data):
+        print("==== scaling DATA ====")
+        scaler = StandardScaler()
 
-    data_list=None
+        train_data = scaler.fit_transform(train_data)
+        val_data = scaler.transform(val_data)
+        test_data = scaler.transform(test_data)
 
-    for path in data_path_list:
-        data = pd.read_csv(path).to_numpy()
-        if data_list is None: 
-            data_list = data
-        else: 
-            data_list = np.concatenate((data_list,data), axis=0)
+        return train_data, val_data, test_data
 
-    x_data = data_list[:,3:]
-    y_data = data_list[:,2:3]
+    def load_data(self):
+        print("==== loading DATA ====")
+        data_path_list = glob('./stride_lab_data/processed_data/*/*')
 
-    train_x, test_x, train_y, test_y =  train_test_split(x_data, y_data, test_size=0.2, random_state=42)
-    train_x, val_x, train_y, val_y =  train_test_split(train_x, train_y, test_size=0.2, random_state=42)
+        data_list=None
 
-    train_x, val_x, test_x = scale_data(train_x, val_x, test_x)
+        for path in data_path_list:
+            data = pd.read_csv(path).to_numpy()
+            if data_list is None: 
+                data_list = data
+            else: 
+                data_list = np.concatenate((data_list,data), axis=0)
 
-    print("train x shape: ", train_x.shape)
-    print("train y shape: ", train_y.shape)
-    print("val x shape: ", val_x.shape)
-    print("val y shape: ", val_y.shape)
-    print("test x shape: ", test_x.shape)
-    print("test y shape: ", test_y.shape)
+        x_data = data_list[:,3:]
+        y_data = data_list[:,2:3]
 
-    global input_size
+        train_x, test_x, train_y, test_y =  train_test_split(x_data, y_data, test_size=0.2, random_state=42)
+        train_x, val_x, train_y, val_y =  train_test_split(train_x, train_y, test_size=0.2, random_state=42)
 
-    input_size = len(train_x[0])
+        train_x, val_x, test_x = self.scale_data(train_x, val_x, test_x)
 
-    train_dataset = tf.data.Dataset.from_tensor_slices((train_x, train_y))
-    train_dataset = train_dataset.shuffle(buffer_size=1024).batch(batch_size)
-    val_dataset = tf.data.Dataset.from_tensor_slices((val_x, val_y))
-    val_dataset = train_dataset.shuffle(buffer_size=1024).batch(batch_size)
-    test_dataset = tf.data.Dataset.from_tensor_slices((test_x, test_y))
-    
-    return train_dataset, val_dataset, test_dataset
+        print("train x shape: ", train_x.shape)
+        print("train y shape: ", train_y.shape)
+        print("val x shape: ", val_x.shape)
+        print("val y shape: ", val_y.shape)
+        print("test x shape: ", test_x.shape)
+        print("test y shape: ", test_y.shape)
 
-def build_cnn_model(input_size):
-    print("==== building DATA ====")
-    model = Sequential()
-    model.add(Conv1D(filters=256, kernel_size=4, padding='same', input_shape=[input_size,1]))
-    model.add(BN())
-    model.add(MaxPooling1D(pool_size=2))
-    model.add(Conv1D(filters=256, kernel_size=4, padding='same'))
-    model.add(BN())
-    model.add(MaxPooling1D(pool_size=2))
-    model.add(Conv1D(filters=256, kernel_size=4, padding='same'))
-    model.add(BN())
-    model.add(MaxPooling1D(pool_size=2))
-    model.add(Conv1D(filters=256, kernel_size=4, padding='same'))
-    model.add(BN())
-    model.add(MaxPooling1D(pool_size=2))
-    model.add(Flatten())
-    model.add(Dense(4096, activation='swish'))
-    model.add(BN())
-    model.add(Dense(4096, activation='swish'))
-    model.add(BN())
-    model.add(Dense(4096, activation='swish'))
-    model.add(BN())
-    model.add(Dense(4096, activation='swish'))
-    model.add(BN())
-    model.add(Dense(1, activation=None))
+        self.input_size = len(train_x[0])
 
-    model.summary()
+        self.train_dataset = tf.data.Dataset.from_tensor_slices((train_x, train_y))
+        self.train_dataset = self.train_dataset.shuffle(buffer_size=1024).batch(batch_size)
+        self.val_dataset = tf.data.Dataset.from_tensor_slices((val_x, val_y))
+        self.val_dataset = self.val_dataset.shuffle(buffer_size=1024).batch(batch_size)
+        self.test_dataset = tf.data.Dataset.from_tensor_slices((test_x, test_y))
 
-    return model
+    def build_cnn_model(self):
+        print("==== building DATA ====")
+        model = Sequential()
+        model.add(Conv1D(filters=256, kernel_size=4, padding='same', input_shape=[input_size,1]))
+        model.add(BN())
+        model.add(MaxPooling1D(pool_size=2))
+        model.add(Conv1D(filters=256, kernel_size=4, padding='same'))
+        model.add(BN())
+        model.add(MaxPooling1D(pool_size=2))
+        model.add(Conv1D(filters=256, kernel_size=4, padding='same'))
+        model.add(BN())
+        model.add(MaxPooling1D(pool_size=2))
+        model.add(Conv1D(filters=256, kernel_size=4, padding='same'))
+        model.add(BN())
+        model.add(MaxPooling1D(pool_size=2))
+        model.add(Flatten())
+        model.add(Dense(4096, activation='swish'))
+        model.add(BN())
+        model.add(Dense(4096, activation='swish'))
+        model.add(BN())
+        model.add(Dense(4096, activation='swish'))
+        model.add(BN())
+        model.add(Dense(4096, activation='swish'))
+        model.add(BN())
+        model.add(Dense(1, activation=None))
 
-def setup_optimizers(learning_rate):
-    l_step_optimizer = Adam(learning_rate=learning_rate)
-    l_step_loss = MSE()
+        model.summary()
+
+        return model
+
+    def setup_optimizers(self):
+        l_step_optimizer = Adam(learning_rate=learning_rate)
+        l_step_loss = MSE()
+
+        return l_step_optimizer, l_step_loss
+
+    def train(self):
+        for epoch in range(epochs):
+            print("epoch " , epoch)
+
+            with tf.GradientTape as tape:
+                logits = model()
+
 
 if __name__ == '__main__':
     setup_gpu()
-    train_dataset, val_dataset, test_dataset = load_data()
+    # train_dataset, val_dataset, test_dataset = load_data()
 
-    l_step_model = build_cnn_model(input_size)
-    setup_optimizers(1.46e-4)
+    # l_step_model = build_cnn_model(input_size)
+    # l_step_optimizer, l_step_loss = setup_optimizers()
 
+    # setup_optimizers(1.46e-4)
+    pass
