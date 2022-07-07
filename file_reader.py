2f1621230188c6db04bc75928c31f49b9a1e559f
Seungmin Jeong <sontneptm2@gmail.com>
2022-07-07 22:19:22 +0900

    update

--- file_reader.py
+++ file_reader.py
@@ -49,5 +49,5 @@ if __name__ == '__main__':
     subject_list = read_files()
     
     for subject in subject_list:
-        if subject.name == "정승민":
+        if subject.name == "최진성":
             subject.find_stride_split_index(type="right")
